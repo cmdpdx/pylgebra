ef1b6f451972b252c22543b462a1c6dbfa2267ef
Colin Douglas <16968564+cmdpdx@users.noreply.github.com>
2018-10-06 12:33:36 -0700

    MULT and ADD simplify() updated

--- operations.py
+++ operations.py
@@ -1,26 +1,41 @@
-_inst = isinstance
+# TODO: make this a function that can accept a list of types to check against
+_isa = isinstance
 from term import Term
 
 class ADD(object):
     def __init__(self, a, b):
         err = False
-        if _inst(a, Term) or _inst(a, ADD):
-            self.a = a
-        elif _inst(a, int) or _inst(a, float):
+        if _isa(a, Term) or _isa(a, ADD) or _isa(a, MULT):
+            self.a = a.clone()
+        elif _isa(a, int) or _isa(a, float):
             self.a = Term(a)
         else:
             err = True
 
-        if _inst(b, Term) or _inst(b, ADD):
-            self.b = b
-        elif _inst(b, int) or _inst(b, float):
+        if _isa(b, Term) or _isa(b, ADD) or _isa(b, MULT):
+            self.b = b.clone()
+        elif _isa(b, int) or _isa(b, float):
             self.b = Term(b)
         else:
             err = True
         
         if err:
-            raise TypeError("{} and {} must be of type int, float, Term, or ADD.".format(a, b))
+            raise TypeError("{} and {} must be of type int, float, Term, ADD, or MULT.".format(a, b))
     
+    def _unpack_add(self, add):
+        """Recursively separates ADDs into their two terms, unpack ADDs along the way.
+
+        Note that no other operators are simplified here; they are just returned with
+        the rest of the terms.
+        """
+        terms = []
+        for addend in (add.a, add.b):
+            if _isa(addend, ADD):
+                terms += self._unpack_add(addend)
+            else:
+                terms.append(addend)
+        return terms
+
     def simplify(self):
         """Combines all like terms within both terms of the add.
 
@@ -28,17 +43,32 @@ class ADD(object):
         All posible like terms are combined. Simplifies the ADD object in place.
         """
         # Collect all the terms (will recursively flatten all sub-ADDs)
+        print("simplifying", self)
         all_terms = self.terms
         terms = []
-        # It's all addition; combine like terms
+        # It's all addition; combine like terms, unless it's a MULT
         for i in range(len(all_terms)):
+            # Skip Nones (they've been combined already)
             if all_terms[i] is None:
                 continue
+            # Simplify MULTs; result is either an ADD or Term. Unpack ADDs and add
+            # the resulting terms to the end of all_terms.
+            if _isa(all_terms[i], MULT):
+                mult = all_terms[i]
+                mult.simplify()
+                all_terms[i] = mult.value
+                if _isa(all_terms[i], ADD):
+                    all_terms += self._unpack_add(all_terms[i])
+                    all_terms[i] = None  # clear the term; it's been simplified/unpacked
+                    continue 
+            # Must be a Term; check all terms past the current one, combining any 
+            # like terms
             t = all_terms[i]
             for j in range(i+1, len(all_terms)):
-                if all_terms[j] is not None and t.like_term(all_terms[j]):
+                if _isa(all_terms[j], Term) and t.like_term(all_terms[j]):
+                    print("found like terms: {} and {}".format(t, all_terms[j]))
                     t.add(all_terms[j])
-                    all_terms[j] = None
+                    all_terms[j] = None  # clear the term; it's been combined
             terms.append(t)
         
         # One term left: save it to a, make b = 0
@@ -61,27 +91,30 @@ class ADD(object):
     
     def distribute(self, factor):
         """Multiply factor to both terms of the sum."""
-        if _inst(factor, Term) or _inst(factor, int) or _inst(factor, float):
-            if _inst(self.a, ADD):
+
+        print("DISTRIBUTING {} over {}".format(factor, self))
+        if _isa(factor, Term) or _isa(factor, int) or _isa(factor, float):
+            if _isa(self.a, ADD):
                 self.a.distribute(factor)
-            elif _inst(self.a, Term):
+            elif _isa(self.a, Term):
                 self.a.multiply(factor)
-            if _inst(self.b, ADD):
+            if _isa(self.b, ADD):
                 self.b.distribute(factor)
-            elif _inst(self.b, Term):
+            elif _isa(self.b, Term):
                 self.b.multiply(factor)
         # If the factor is an ADD, then we'll have to distribute each term
         # of the sum over the ADD factor: (a+b)(c+d) = (a+b)*c + (a+b)*d
         # Those new products become the terms of this ADD
-        elif _inst(factor, ADD):
-            a = factor.clone()
-            b = factor.clone()
-            a.distribute(self.a)
-            b.distribute(self.b)
-            a.simplify()
-            b.simplify()
-            self.a = a
-            self.b = b
+        elif _isa(factor, ADD):
+            factor_a = factor.clone()
+            factor_b = factor.clone()
+            factor_a.distribute(self.a)
+            factor_b.distribute(self.b)
+            factor_a.simplify()
+            factor_b.simplify()
+            self.a = factor_a.value
+            self.b = factor_b.value
+        print("...finished distributing:", self)
         self.simplify()
             
     def clone(self):
@@ -92,13 +125,14 @@ class ADD(object):
             
     @property
     def terms(self):
-        """Get a flat list of all terms in both parts of the ADD."""
+        """Get a flat list of all terms in both addends of the ADD."""
         t = []
-        if _inst(self.a, Term):
+        if _isa(self.a, Term) or _isa(self.a, MULT):
             t.append(self.a)
-        else:
+        elif _isa(self.a, ADD):
             t += self.a.terms
-        if _inst(self.b, Term):
+        
+        if _isa(self.b, Term) or _isa(self.b, MULT):
             t.append(self.b)
         else:
             t += self.b.terms
@@ -116,12 +150,12 @@ class ADD(object):
 
     def __str__(self):
         s = []
-        if _inst(self.a, ADD):
+        if _isa(self.a, ADD):
             s.append("({})".format(str(self.a)))
         else:
             s.append(str(self.a))
         s.append(" + ")
-        if _inst(self.b, ADD):
+        if _isa(self.b, ADD):
             s.append("({})".format(str(self.b)))
         else:
             s.append(str(self.b))
@@ -132,16 +166,16 @@ class ADD(object):
 class MULT(object):
     def __init__(self, a, b):
         err = False
-        if _inst(a, Term) or _inst(a, ADD) or _inst(a, MULT):
-            self.a = a
-        elif _inst(a, int) or _inst(a, float):
+        if _isa(a, Term) or _isa(a, ADD) or _isa(a, MULT):
+            self.a = a.clone()
+        elif _isa(a, int) or _isa(a, float):
             self.a = Term(a)
         else:
             err = True
 
-        if _inst(b, Term) or _inst(b, ADD) or _inst(a, MULT):
-            self.b = b
-        elif _inst(b, int) or _inst(b, float):
+        if _isa(b, Term) or _isa(b, ADD) or _isa(a, MULT):
+            self.b = b.clone()
+        elif _isa(b, int) or _isa(b, float):
             self.b = Term(b)
         else:
             err = True
@@ -151,36 +185,55 @@ class MULT(object):
     
     # TODO: finish simplify
     def simplify(self):
-        if _inst(self.a, Term):
-            if _inst(self.b, Term):
-                self.a.multiply(self.b)
-                self.b = Term(1)
-            elif _inst(self.b, ADD):
-                self.b.distribute(self.a)
-                self.a = Term(1)
-            # what if b is a MULT?
-        # what if a is an ADD? or a MULT?
+        """Perform the multiplication, simplify results as much as possible.
+
+        Multiplication can always be performed, and so the result should never
+        be a MULT; possible results are Term and ADD. For consistency, the result
+        is stored in self.a, with the multiplicative identity in self.b.
+        """
+        # simplfiy inner groups first (PEMDAS)
+        # Save the results in local a and b variables
+        if _isa(self.a, ADD) or _isa(self.a, MULT):
+            self.a.simplify()
+            a = self.a.value    # a is either a Term or ADD
+        else:
+            a = self.a
+        if _isa(self.b, ADD) or _isa(self.b, MULT):
+            self.b.simplify()
+            b = self.b.value    # b is either a Term or ADD
+        else:
+            b = self.b
+
+        # There should only be four possibilities at this point:
+        # Term x Term, Term x ADD, ADD x Term, ADD x ADD
+        if _isa(a, Term) and _isa(b, Term):
+            a.multiply(b)
+            self.a, self.b = a, Term(1)
+        elif _isa(a, Term) and _isa(b, ADD):
+            b.simplify()
+            b.distribute(a)
+            # TODO: determine if the ADD is really just a Term
+            self.a, self.b = b, Term(1)
+        elif _isa(a, ADD) and _isa(b, Term):
+            a.simplify()
+            a.distribute(b)
+            self.a, self.b = a, Term(1)
+        # (a+b) * (c+d) = a(c+d) + b(c+d)
+        elif _isa(a, ADD) and _isa(b, ADD):
+            a.simplify()
+            b.simplify()
+            # TODO: FOIL!!
+            print("a =", a)
+            print("b =", b)
+            first = b.distribute(a.a)
+            second = b.distribute(a.b)
+            self.a, self.b = ADD(first, second), Term(1)
     
-    # TODO: finish value
     @property
     def value(self):
-        if _inst(self.a, Term) and _inst(self.b, Term):
-            return Term.product(self.a, self.b)
-        if _inst(self.a, Term):
-            if _inst(self.b, ADD):
-                b = self.b.clone()
-                b.distribute(self.a)
-                return b.value
-            if _inst(self.b, MULT):
-                # depends on what the pieces of the MULT are...
-                pass
-        if _inst(self.b, Term):
-            if _inst(self.a, ADD):
-                a = self.a.clone()
-                a.distribute(self.b)
-                return a.value
-            if _inst(self.a, MULT):
-                pass
+        if not self.b.is_one():
+            self.simplify()
+        return self.a.clone()
 
     def clone(self):
         a = self.a.clone()
@@ -189,12 +242,12 @@ class MULT(object):
 
     def __str__(self):
         s = []
-        if _inst(self.a, ADD) or _inst(self.a, MULT):
+        if _isa(self.a, ADD) or _isa(self.a, MULT):
             s.append("({})".format(str(self.a)))
         else:
             s.append(str(self.a))
         s.append(" * ")
-        if _inst(self.b, ADD) or _inst(self.b, MULT):
+        if _isa(self.b, ADD) or _isa(self.b, MULT):
             s.append("({})".format(str(self.b)))
         else:
             s.append(str(self.b))
